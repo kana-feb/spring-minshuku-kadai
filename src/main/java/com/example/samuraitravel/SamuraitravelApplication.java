f3a995ec3c4c230fe04e4ab671406ced10add171
kana-feb <canary.sug.02@gmail.com>
2025-08-06 00:32:38 +0900

    first commit

---
+++ src/main/java/com/example/samuraitravel/SamuraitravelApplication.java
@@ -0,0 +1,15 @@
+
+package com.example.samuraitravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SamuraitravelApplication {
+ //ここはmainの実行クラスです。SpringAppフレームワークでSamuraitravelApplicationクラスを実行する
+	public static void main(String[] args) {
+		SpringApplication.run(SamuraitravelApplication.class, args);
+	}
+
+}
