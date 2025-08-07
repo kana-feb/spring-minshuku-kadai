bcf4fc58ecb6bd0e853902248752df098df55cc9
kana-feb <canary.sug.02@gmail.com>
2025-08-07 22:51:14 +0900

    commit

--- src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
+++ src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
@@ -33,8 +33,7 @@ public class WebSecurityConfig {
                 .logoutSuccessUrl("/?loggedOut")  // ログアウト時のリダイレクト先URL
                 .permitAll()
                         )
-            .csrf().ignoringRequestMatchers("/stripe/webhook");
-            
+        .csrf(csrf -> csrf.ignoringRequestMatchers("/stripe/webhook"));
             
         return http.build();
     }
